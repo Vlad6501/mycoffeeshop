9aa5b84a5ee7980ab66969bb83e956f1be7e0158
vladk <vladklimenko65@gmail.com>
2020-02-20 19:23:16 +0200

    commit del idea

--- target/classes/db/migration/V2__Add_admin.sql
+++
@@ -1,8 +0,0 @@
-insert into usr (id, active, password, username, email)
-    values (1, true, '$2a$08$Hgepekisa27geJOggidcdenredIRs8lYQYUZsvApYMGHLqQmh.fO.', 'admin', 'vladklimenko65@gmail.com');
-
-insert into information (id, email, firstname, lastname, patronymic, phone, inf_id)
-    values (1, '', '', '', '', '', 1);
-
-insert into user_role (user_id, roles)
-    values (1, 'USER'), (1, 'ADMIN');
--- target/test-classes/create-user-after.sql
+++
@@ -1,3 +0,0 @@
-delete from user_role;
-delete from information;
-delete from usr;
